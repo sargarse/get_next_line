cff0e293bc6f73f4309c81a0eb989b6d2622ac55
Sara García <40412452+sargarse@users.noreply.github.com>
2023-12-29 21:04:10 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -5,61 +5,88 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sgarcia3 <sgarcia3@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/28 19:44:45 by sgarcia3          #+#    #+#             */
-/*   Updated: 2023/12/28 19:45:36 by sgarcia3         ###   ########.fr       */
+/*   Created: 2023/12/05 22:47:28 by sgarcia3          #+#    #+#             */
+/*   Updated: 2023/12/09 02:14:58 by sgarcia3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
-char	*read_line(char **buffer, int fd)
+void	ft_bzero(void *s, size_t n)
 {
-	char	*line;
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		*((unsigned char *)s + i) = 0;
+		i++;
+	}
+}
+
+ssize_t	read_fd(int fd, char **buff_ptr)
+{
 	ssize_t	ctrl;
+	char	*buff;
+
+	buff = *buff_ptr;
+	ctrl = read(fd, buff, BUFFER_SIZE);
+	if (ctrl < 0)
+	{
+		free(buff);
+		buff = NULL;
+		return (-1);
+	}
+	if (ctrl > 0)
+		buff[ctrl] = '\0';
+	*buff_ptr = buff;
+	return (ctrl);
+}
+
+char	*read_line(char **buff, int fd)
+{
 	char	*ptrln;
+	char	*line = "";
+	ssize_t	ctrl;
 	char	*tmp;
-	
-	ctrl = 1;
-	line = (char *)malloc(sizeof(char));
-	if (!line)
-	{
+
+	ctrl = read_fd(fd, &*buff);
+	if (ctrl < 1) {
 		return (NULL);
-		free(*buffer);
 	}
-	line = "";
-	while (ft_strchr(*buffer, '\n') == NULL)
+	while ((ptrln = ft_strchr(*buff, '\n')) == NULL && ctrl > 0)
 	{
-		line = ft_strjoin(line, *buffer);
-		ctrl = read(fd, *buffer, BUFFER_SIZE);
-		if (ctrl == -1 || ctrl == 0)
-		{
-			free(line);
+		line = ft_strjoin(line, *buff);
+		ctrl = read_fd(fd, &*buff);
+		if (ctrl == -1)
 			return (NULL);
-		}
 	}
-	if ((ptrln = ft_strchr(*buffer, '\n')))
+	if ((ptrln = ft_strchr(*buff, '\n')))
 	{
-		tmp = ft_substr(*buffer, 0, ptrln - *buffer);
+		tmp = ft_substr(*buff, 0, ptrln - *buff);
 		line = ft_strjoin(line, tmp);
-		*buffer = ptrln + 1;
-		free(tmp);
+		ft_memmove(*buff, ptrln + 1, ft_strlen(ptrln + 1));
+		ft_bzero(*buff + ft_strlen(ptrln + 1), BUFFER_SIZE - ft_strlen(ptrln + 1));
 	}
+	if (ctrl < 0)
+		return (NULL);
 	return (line);
 }
-char	*get_next_line(int fd)
-{
-	static char *buffer;
-	char 		*line;
-	
-	if (fd < 0 || BUFFER_SIZE < 1)
+
+char *get_next_line(int fd) {
+	static char *buff;
+	char *line;
+
+	if (fd == -1 || BUFFER_SIZE < 1)
 		return (NULL);
-	if (!buffer)
+	if (!buff)
 	{
-		buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-		if (!buffer)
+		buff = (char *) malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (!buff)
 			return (NULL);
-		buffer[0] = '\0';
+		buff[0] = '\0';
 	}
-	line = read_line(&buffer, fd);
+	line = read_line(&buff, fd);
 	return (line);
 }
