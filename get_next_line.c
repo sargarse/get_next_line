036548e362bcf31ebabe9ec31232240882ed5a9a
sargarse <sargarse@gmail.com>
2024-01-27 19:56:26 +0100

    Listo para entregar, eliminar archivos

--- get_next_line.c
+++ get_next_line.c
@@ -31,7 +31,51 @@ char	*get_buffer(char *buff, ssize_t *position)
 	return (line);
 }
 
+char	*manage_aux(char *buff_aux, char *buff, char *line, ssize_t *position)
+{
+	ssize_t	len;
+	char	*temp;
+
+	len = 0;
+	while (*(buff_aux + len) && *(buff_aux + len) != '\n')
+		len++;
+	if (*(buff_aux + len) == '\n')
+		len++;
+	if (len < BUFFER_SIZE + 1)
+		ft_strlcpy_gnl(buff, buff_aux + len, (BUFFER_SIZE - len + 1));
+	*temp = (char *)malloc(sizeof(char) * (len + 1));
+	if (!temp)
+		return (NULL);
+	ft_strlcpy_gnl(temp, buff_aux, len + 1);
+	line = ft_strjoin_gnl(line, temp, position);
+	return (line);
+}
+
 char	*line_exceeds_buff(char *line, char *buff, ssize_t *position, int fd)
+{
+	char	buff_aux[BUFFER_SIZE + 1];
+	ssize_t	bytes_read;
+
+	while (*position == 0)
+	{
+		ft_bzero(buff_aux, BUFFER_SIZE + 1);
+		bytes_read = read(fd, buff_aux, BUFFER_SIZE);
+		if (bytes_read == -1 || (bytes_read == 0 && ft_strlen_gnl(line) == 0))
+		{
+			free(line);
+			return (NULL);
+		}
+		line = manage_aux(buff_aux, buff, line, position);
+		if (!line)
+			return (NULL);
+		if (bytes_read < BUFFER_SIZE)
+			*position = -1;
+	}
+	*position = 0;
+	return (line);
+}
+
+/*char	*line_exceeds_buff(char *line, char *buff, ssize_t *position, int fd)
 {
 	char	buff_aux[BUFFER_SIZE + 1];
 	ssize_t	bytes_read;
@@ -64,7 +108,7 @@ char	*line_exceeds_buff(char *line, char *buff, ssize_t *position, int fd)
 	}
 	*position = 0;
 	return (line);
-}
+}*/
 
 char	*get_next_line(int fd)
 {
